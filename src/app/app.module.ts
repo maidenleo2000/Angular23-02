9385abf163c7b33b9a74a056554cb9f498a243a4
Leandro Martello <maidenleo2000@gmail.com>
2023-04-19 22:20:26 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
+//?Primero van las importaciones de Angular, lo que es oficial.
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//?2 - Van las importaciones de modulos externos
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+//?3- Por ultimo nuestro codigo
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { HeroComponent } from './hero/hero.component';
+import { ListComponent } from './list/list.component';
+
+
+
+
+@NgModule({
+  declarations:[
+    HeroComponent,
+    ListComponent
+  ],
+  exports:[
+    HeroComponent,
+    ListComponent
+  ],
+  imports:[
+    //!Este modulo es necesario para que funcione NGIF NGFOR NGTEMPLATE etc.. Si no lo importo da error.
+    CommonModule,
+  ]
+})
+export class HeroesModule {
+
+}
