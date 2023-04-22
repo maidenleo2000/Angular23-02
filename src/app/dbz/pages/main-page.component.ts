2235d44932ce20734beaf0e2cf06d21f28bb071c
Leandro Martello <maidenleo2000@gmail.com>
2023-04-22 09:26:08 -0300

    añadido el servicio

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -8,29 +9,36 @@ import { Character } from '../interfaces/character.interface';
 
 export class MainPageComponent {
 //?Aca aprendemos @INPUT() mandando este array characters para el componente list.component.ts. Como este es el PADRE vamos a ir al main-page.component.html y en el TAG de dbz-list se le agrega un corchete con la propiedad DEL HIJO (characterList) y se iguala al valor del PADRE (characters)
-  public characters: Character[] = [
-    {
-      name: 'Krillin',
-      power: 1000
-    },
-    {
-      name: 'Goku',
-      power: 9500
-    },
-    {
-      name:'Vegeta',
-      power: 7500
-    }
-  ];
 
-  oneNewCharacterRecibido(character: Character):void {
-    // console.log('Main Page');
-    // console.log(character);
-    this.characters.push(character);
-  }
+//?Esta es la inyeccion de dependencia para traer la info de service
+constructor(public dbzService: DbzService ){
 
-  onDeleteCharacter(index:number){
-    this.characters.splice(index, 1);
-  }
+}
+
+//!Esta parte está ahora en el servicio
+  // public characters: Character[] = [
+  //   {
+  //     name: 'Krillin',
+  //     power: 1000
+  //   },
+  //   {
+  //     name: 'Goku',
+  //     power: 9500
+  //   },
+  //   {
+  //     name:'Vegeta',
+  //     power: 7500
+  //   }
+  // ];
+
+  // oneNewCharacterRecibido(character: Character):void {
+  //   // console.log('Main Page');
+  //   // console.log(character);
+  //   this.characters.push(character);
+  // }
+
+  // onDeleteCharacter(index:number){
+  //   this.characters.splice(index, 1);
+  // }
 
 }
